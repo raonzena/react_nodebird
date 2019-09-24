b2fb351b419f9352571decf3b284927c53c174ec
amy <amy@soomgo.com>
2019-09-24 20:33:59 +0900

    add source

--- ch1/front/sagas/user.js
+++ ch1/front/sagas/user.js
@@ -2,19 +2,19 @@ import { all, fork, takeLatest } from 'redux-saga/effects';
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
 
 function loginAPI() {
-
+    // 서버에 요청을 보내는 부분
 }
 
 function* login() {
     try {
         yield call(loginAPI);
-        yield put({
-            type: LOG_IN_SUCCESS
+        yield put({ // put은 dispatch 동일
+            type: LOG_IN_SUCCESS,
         });
-    } catch (e) {
+    } catch (e) { // loginAPI 실패
         console.error(e);
         yield put({
-            type: LOG_IN_FAILURE
+            type: LOG_IN_FAILURE,
         });
     }
 }
